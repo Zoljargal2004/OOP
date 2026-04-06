1ea2a10f62d3fc2de50c3ca9100ab0d028c922d0
dulguuun <dulguun421111@gmail.com>
2026-04-06 22:31:52 +0800

    kvadrat dulguun

--- lab6.cpp
+++ lab6.cpp
@@ -51,15 +51,42 @@ public:
     }
 
     void show() override{
-        cout <<"Нэр: "<<name<<endl;
-        cout <<"Төвийн координат: ("<<x<<", "<<y<<")\n";
-        cout <<"Радиус: "<<r<<endl;
+        cout <<": "<<name<<endl;
+        cout <<"Tuviin coordinate: ("<<x<<", "<<y<<")\n";
+        cout <<"Radius: "<<r<<endl;
     }
 };
 
 // 2. Квадрат class
 class kvadrat : public Shape2D{
+private:
+    double x, y; //zuun deed talin tseg
+    double a; //taliin urt
 
+public:
+    kvadrat(string n, double x, double y, double side)
+        : Shape2D(n){
+        this->x = x;
+        this->y = y;
+        a = side;
+    }
+
+    double area() override{
+        return a * a;
+    }
+
+    double perimeter() override{
+        return 4 * a;
+    }
+
+    void show() override{
+        cout<<"Name:"<<name<<endl;
+        cout<<"Shuluun:("<<x<<","<<y<<")"
+             <<"("<<x+a<<","<<y<<")"
+             <<"("<<x+a<<","<<y-a<<")"
+             <<"("<<x<<","<<y-a<<")"<<endl;
+        cout<<"Side:"<<a<<endl;
+    }
 };
 
 // 3. Зөв гурвалжин class
@@ -70,12 +97,13 @@ class zuvGurvaljin : public Shape2D{
 int main(){
     Shape* shapes[3];
 
-    shapes[0] = new toirog("Тойрог", 0, 0, 8);
+    shapes[0] = new toirog("Toirog", 0, 0, 8);
+    shapes[1] = new kvadrat("kvadrat", 0, 0, 5);
 
     for (int i = 0; i < 3; i++){
         shapes[i]->show();
-        cout<<"Талбай: "<<shapes[i]->area()<<endl; 
-        cout<<"Периметр: "<<shapes[i]->perimeter()<<endl;
+        cout<<"Talbai: "<<shapes[i]->area()<<endl; 
+        cout<<"Perimeter: "<<shapes[i]->perimeter()<<endl;
     }
 
     // санах ойг чөлөөлөх
